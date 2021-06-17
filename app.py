6da17dd879de813015a172d956c59e17e9f5eb52
Bruno Esparza <bruno.be81@gmail.com>
2021-06-16 21:48:08 -0500

    feat(app): add json response

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker!'
+    return jsonify({
+        "Universidad": "UTPL",
+        "Curso": "Procesos de Ingeniería de Software",
+        "Alumno": "Bruno Alexander Esparza Carchi",
+        "Período": "Abr/Ago 2021",
+        "Lenguaje de programación preferido": "Javascript",
+        "Aspiración PostGraduación": "Unirme a una startup o empresa que se encuentre en aceleración, certificarme como Google Web Developer, y seguir una certificación de GCP o AWS. Practicar algoritmos difíciles."
+    })
